37e5bae16d1ef96f127171a072db439ffa8fe36a
agnesoctamia <agnes.oktamia@si.ukdw.ac.id>
2016-04-07 18:05:28 +0700

    tambah view sertifikasi

---
+++ application/views/sertifikasi/index.php
@@ -0,0 +1,235 @@
+<head><title>Sertifikasi PPLK</title></head>
+<div class="container">
+  <br>
+    <p class="lead">
+      <h2 class="featurette-heading" style="text-align: center;">Sertifikasi PPLK</h2>
+      <img class="featurette-image img-responsive center-block" style="width:600px; height:330px:-70px;" src="<?php echo base_url('images/lab-1.jpg')?>">
+      <hr >
+      <H4 style="margin-left:15px;">PPLK UKDW menyediakan berbagai macam sertifikasi yang dapat diikuti, antara lain : 
+    </H4>  
+    
+    <ol>    
+      <div style="width:45%;">
+        <div class="container">
+  <h2 style="font-size: 15px;">1. OPERATOR</h2>   
+  <table class="table table-hover">
+    <thead>
+      <tr>
+        <th>Sertifikasi</th>
+        <th>Biaya Assesmen</th>
+        <th>Biaya Sertifikasi</th>
+        <th>Biaya Resertifikasi</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>JUNIOR OFFICE OPERATOR</td>
+        <td>Rp 600.000 </td>
+        <td>Rp 350.000</td>
+        <td>Rp 650.000</td>
+      </tr>
+      <tr>
+        <td>HELP DESK</td>
+        <td>RP 900.000</td>
+        <td>RP 350.000</td>
+        <td>RP 800.000</td>
+      </tr>
+      <tr>
+        <td>SENIOR OFFICE OPERATOR</td>
+        <td>RP 900.000</td>
+        <td>RP 350.000</td>
+        <td>RP 800.000</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+<div class="container">
+  <h2 style="font-size: 15px;">2. PROGRAMMING</h2>
+  <table class="table table-hover">
+    <thead>
+      <tr>
+        <th>Sertifikasi</th>
+        <th>Biaya Assesmen</th>
+        <th>Biaya Sertifikasi</th>
+        <th>Biaya Resertifikasi</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>JUNIOR PROGRAMMER</td>
+        <td>Rp 600.000 </td>
+        <td>Rp 350.000</td>
+        <td>Rp 650.000</td>
+      </tr>
+      <tr>
+        <td>PROGRAMMER</td>
+        <td>RP 1.000.000</td>
+        <td>RP 350.000</td>
+        <td>RP 950.000</td>
+      </tr>
+      <tr>
+        <td>ANALIST PROGRAMMER</td>
+        <td>RP 3.000.000</td>
+        <td>RP 350.000</td>
+        <td>RP 1.850.000</td>
+      </tr>
+      <tr>
+        <td>JUNIOR WEB PROGRAMMER</td>
+        <td>Rp 600.000 </td>
+        <td>Rp 350.000</td>
+        <td>Rp 650.000</td>
+      </tr>
+      <tr>
+        <td>WEB DESIGNER</td>
+        <td>Rp 1.000.000 </td>
+        <td>Rp 350.000</td>
+        <td>Rp 950.000</td>
+      </tr>
+      <tr>
+        <td>JUNIOR MULTIMEDIA PROGRAMMER</td>
+        <td>Rp 600.000 </td>
+        <td>Rp 350.000</td>
+        <td>Rp 650.000</td>
+      </tr>
+      <tr>
+        <td>MULTIMEDIA PROGRAMMER</td>
+        <td>Rp 1.000.000 </td>
+        <td>Rp 350.000</td>
+        <td>Rp 950.000</td>
+      </tr>
+      <tr>
+        <td>MOBILE PROGRAMMER</td>
+        <td>Rp 1.500.000 </td>
+        <td>Rp 100.000</td>
+        <td>Rp 1.000.000</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+<div class="container">
+<h2 style="font-size: 15px;">3. TECHNICAL</h2>   
+  <table class="table table-hover">
+    <thead>
+      <tr>
+        <th>Sertifikasi</th>
+        <th>Biaya Assesmen</th>
+        <th>Biaya Sertifikasi</th>
+        <th>Biaya Resertifikasi</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>JUNIOR TECHNICAL SUPPORT</td>
+        <td>Rp 600.000 </td>
+        <td>Rp 350.000</td>
+        <td>Rp 650.000</td>
+      </tr>
+      <tr>
+        <td>TECHNICAL SUPPORT</td>
+        <td>RP 2.000.000</td>
+        <td>RP 350.000</td>
+        <td>RP 1.350.000</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+<div class="container">
+  <h2 style="font-size: 15px;">4. NETWORK</h2>   
+  <table class="table table-hover">
+    <thead>
+      <tr>
+        <th>Sertifikasi</th>
+        <th>Biaya Assesmen</th>
+        <th>Biaya Sertifikasi</th>
+        <th>Biaya Resertifikasi</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>JUNIOR NETWORK ADMINISTRATOR</td>
+        <td>Rp 600.000 </td>
+        <td>Rp 350.000</td>
+        <td>Rp 650.000</td>
+      </tr>
+      <tr>
+        <td>NETWORK ADMINISTRATOR</td>
+        <td>RP 1.000.000</td>
+        <td>RP 350.000</td>
+        <td>RP 950.000</td>
+      </tr>
+      <tr>
+        <td>NETWORK ENGINEER</td>
+        <td>RP 2.000.000</td>
+        <td>RP 350.000</td>
+        <td>RP 1.350.000</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+<div class="container">
+  <h2 style="font-size: 15px;">5. ADMINISTRATOR</h2>   
+  <table class="table table-hover">
+    <thead>
+      <tr>
+        <th>Sertifikasi</th>
+        <th>Biaya Assesmen</th>
+        <th>Biaya Sertifikasi</th>
+        <th>Biaya Resertifikasi</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>JUNIOR SISTEM ADMINISTRATOR</td>
+        <td>Rp 600.000 </td>
+        <td>Rp 350.000</td>
+        <td>Rp 650.000</td>
+      </tr>
+      <tr>
+        <td>SISTEM ADMINISTRATOR</td>
+        <td>RP 1.000.000</td>
+        <td>RP 350.000</td>
+        <td>RP 950.000</td>
+      </tr>
+      <tr>
+        <td>SISTEM ENGINEER</td>
+        <td>RP 2.000.000</td>
+        <td>RP 350.000</td>
+        <td>RP 950.000</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+<div class="container">
+<h2 style="font-size: 15px;">6. MULTIMEDIA</h2>   
+  <table class="table table-hover">
+    <thead>
+      <tr>
+        <th>Sertifikasi</th>
+        <th>Biaya Assesmen</th>
+        <th>Biaya Sertifikasi</th>
+        <th>Biaya Resertifikasi</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>JUNIOR DIGITAL ARTIST</td>
+        <td>Rp 600.000 </td>
+        <td>Rp 350.000</td>
+        <td>Rp 650.000</td>
+      </tr>
+      <tr>
+        <td>DIGITAL ARTIST</td>
+        <td>RP 1.000.000</td>
+        <td>RP 350.000</td>
+        <td>RP 950.000</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+  </ol>
+</div>
