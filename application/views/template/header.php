34f2f8d8125c79b9340a3d7c9942f1887f623819
Senna Christanto <senna.christanto@ti.ukdw.ac.id>
2016-04-09 13:48:24 +0700

    dropdown button di navbar kalo ukuran laya kecil (mobile) nb: kurang script bwt collapse up nya, on going

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -45,6 +45,12 @@
     <div class="container">
       <nav class="navbar navbar-default">
         <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
           <a class="navbar-brand" href="<?php echo base_url('')?>">PPLK</a>
         </div>
             <?php
