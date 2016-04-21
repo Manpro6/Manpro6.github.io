b5ff5f037791c918d44bb8ef32027bbf08cf447d
agnesoctamia <agnes.oktamia@si.ukdw.ac.id>
2016-04-21 19:49:07 +0700

    update view

--- application/views/sertifikasi/index.php
+++ application/views/sertifikasi/index.php
@@ -3,56 +3,97 @@
   <br>
     <p class="lead">
       <h2 class="featurette-heading" style="text-align: center;">Sertifikasi PPLK</h2>
-      <hr>
-      <img class="featurette-image img-responsive center-block" style="width:600px; height:330px:-70px;" src="<?php echo base_url('images/lab-1.jpg')?>">
-      <br><H4 style="margin-left:15px;">PPLK UKDW menyediakan berbagai macam sertifikasi yang dapat diikuti, antara lain : 
+      <div class="container">
+  <br>
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+      <li data-target="#myCarousel" data-slide-to="3"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+      <div class="item active">
+        <img src="<?php echo base_url('images/netengginer.jpg')?>" alt="Chania" width="460" height="345">
+      </div>
+
+      <div class="item">
+        <img src="<?php echo base_url('images/sys.jpg')?>" alt="Chania" width="460" height="345">
+      </div>
+    
+      <div class="item">
+        <img src="<?php echo base_url('images/multimedia_designs.jpg')?>" alt="Flower" width="460" height="345">
+      </div>
+
+      <div class="item">
+        <img src="<?php echo base_url('images/IMG_0126.jpg')?>" alt="Flower" width="460" height="345">
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+      
+      <hr >
+      <H4 style="margin-left:15px;">PPLK UKDW menyediakan berbagai macam sertifikasi yang dapat diikuti, antara lain : 
     </H4>  
     
     <ol>    
-      <div style="width:45%;">
-        <div class="container">
+      <div style="width:90%;">
+        <div class="table-responsive">
   <h2 style="font-size: 15px;">1. OPERATOR</h2>   
   <table class="table table-hover">
     <thead>
       <tr>
-        <th style="width:40%;">Sertifikasi</th>
-        <th style="width:20%;">Biaya Assesmen</th>
-        <th style="width:20%;">Biaya Sertifikasi</th>
-        <th style="width:20%;">Biaya Resertifikasi</th>
+        <th style="background-color: #1E90FF;">Sertifikasi</th>
+        <th style="background-color: #1E90FF;">Biaya Assesmen</th>
+        <th style="background-color: #1E90FF;">Biaya Sertifikasi</th>
+        <th style="background-color: #1E90FF;">Biaya Resertifikasi</th>
       </tr>
     </thead>
     <tbody>
       <tr>
-        <td>JUNIOR OFFICE OPERATOR</td>
+        <td>JUNIOR OFFICE OPERATOR  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
         <td>Rp 600.000 </td>
         <td>Rp 350.000</td>
         <td>Rp 650.000</td>
       </tr>
       <tr>
         <td>HELP DESK</td>
-        <td>RP 900.000</td>
-        <td>RP 350.000</td>
-        <td>RP 800.000</td>
+        <td>Rp 900.000</td>
+        <td>Rp 350.000</td>
+        <td>Rp 800.000</td>
       </tr>
       <tr>
         <td>SENIOR OFFICE OPERATOR</td>
-        <td>RP 900.000</td>
-        <td>RP 350.000</td>
-        <td>RP 800.000</td>
+        <td>Rp 900.000</td>
+        <td>Rp 350.000</td>
+        <td>Rp 800.000</td>
       </tr>
     </tbody>
   </table>
 </div>
 
-<div class="container">
+<div class="table-responsive">
   <h2 style="font-size: 15px;">2. PROGRAMMING</h2>
   <table class="table table-hover">
     <thead>
       <tr>
-        <th style="width:40%;">Sertifikasi</th>
-        <th style="width:20%;">Biaya Assesmen</th>
-        <th style="width:20%;">Biaya Sertifikasi</th>
-        <th style="width:20%;">Biaya Resertifikasi</th>
+        <th style="background-color: #1E90FF;">Sertifikasi</th>
+        <th style="background-color: #1E90FF;">Biaya Assesmen</th>
+        <th style="background-color: #1E90FF;">Biaya Sertifikasi</th>
+        <th style="background-color: #1E90FF;">Biaya Resertifikasi</th>
       </tr>
     </thead>
     <tbody>
@@ -64,15 +105,15 @@
       </tr>
       <tr>
         <td>PROGRAMMER</td>
-        <td>RP 1.000.000</td>
-        <td>RP 350.000</td>
-        <td>RP 950.000</td>
+        <td>Rp 1.000.000</td>
+        <td>Rp 350.000</td>
+        <td>Rp 950.000</td>
       </tr>
       <tr>
         <td>ANALIST PROGRAMMER</td>
-        <td>RP 3.000.000</td>
-        <td>RP 350.000</td>
-        <td>RP 1.850.000</td>
+        <td>Rp 3.000.000</td>
+        <td>Rp 350.000</td>
+        <td>Rp 1.850.000</td>
       </tr>
       <tr>
         <td>JUNIOR WEB PROGRAMMER</td>
@@ -108,43 +149,43 @@
   </table>
 </div>
 
-<div class="container">
+<div class="table-responsive">
 <h2 style="font-size: 15px;">3. TECHNICAL</h2>   
   <table class="table table-hover">
     <thead>
       <tr>
-        <th style="width:40%;">Sertifikasi</th>
-        <th style="width:20%;">Biaya Assesmen</th>
-        <th style="width:20%;">Biaya Sertifikasi</th>
-        <th style="width:20%;">Biaya Resertifikasi</th>
+        <th style="background-color: #1E90FF;">Sertifikasi</th>
+        <th style="background-color: #1E90FF;">Biaya Assesmen</th>
+        <th style="background-color: #1E90FF;">Biaya Sertifikasi</th>
+        <th style="background-color: #1E90FF;">Biaya Resertifikasi</th>
       </tr>
     </thead>
     <tbody>
       <tr>
-        <td>JUNIOR TECHNICAL SUPPORT</td>
+        <td>JUNIOR TECHNICAL SUPPORT &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
         <td>Rp 600.000 </td>
         <td>Rp 350.000</td>
         <td>Rp 650.000</td>
       </tr>
       <tr>
         <td>TECHNICAL SUPPORT</td>
-        <td>RP 2.000.000</td>
-        <td>RP 350.000</td>
-        <td>RP 1.350.000</td>
+        <td>Rp 2.000.000</td>
+        <td>Rp 350.000</td>
+        <td>Rp 1.350.000</td>
       </tr>
     </tbody>
   </table>
 </div>
 
-<div class="container">
+<div class="table-responsive">
   <h2 style="font-size: 15px;">4. NETWORK</h2>   
   <table class="table table-hover">
     <thead>
       <tr>
-        <th style="width:40%;">Sertifikasi</th>
-        <th style="width:20%;">Biaya Assesmen</th>
-        <th style="width:20%;">Biaya Sertifikasi</th>
-        <th style="width:20%;">Biaya Resertifikasi</th>
+        <th style="background-color: #1E90FF;">Sertifikasi</th>
+        <th style="background-color: #1E90FF;">Biaya Assesmen</th>
+        <th style="background-color: #1E90FF;">Biaya Sertifikasi</th>
+        <th style="background-color: #1E90FF;">Biaya Resertifikasi</th>
       </tr>
     </thead>
     <tbody>
@@ -156,77 +197,77 @@
       </tr>
       <tr>
         <td>NETWORK ADMINISTRATOR</td>
-        <td>RP 1.000.000</td>
-        <td>RP 350.000</td>
-        <td>RP 950.000</td>
+        <td>Rp 1.000.000</td>
+        <td>Rp 350.000</td>
+        <td>Rp 950.000</td>
       </tr>
       <tr>
         <td>NETWORK ENGINEER</td>
-        <td>RP 2.000.000</td>
-        <td>RP 350.000</td>
-        <td>RP 1.350.000</td>
+        <td>Rp 2.000.000</td>
+        <td>Rp 350.000</td>
+        <td>Rp 1.350.000</td>
       </tr>
     </tbody>
   </table>
 </div>
 
-<div class="container">
+<div class="table-responsive">
   <h2 style="font-size: 15px;">5. ADMINISTRATOR</h2>   
   <table class="table table-hover">
     <thead>
       <tr>
-        <th style="width:40%;">Sertifikasi</th>
-        <th style="width:20%;">Biaya Assesmen</th>
-        <th style="width:20%;">Biaya Sertifikasi</th>
-        <th style="width:20%;">Biaya Resertifikasi</th>
+        <th style="background-color: #1E90FF;">Sertifikasi</th>
+        <th style="background-color: #1E90FF;">Biaya Assesmen</th>
+        <th style="background-color: #1E90FF;">Biaya Sertifikasi</th>
+        <th style="background-color: #1E90FF;">Biaya Resertifikasi</th>
       </tr>
     </thead>
     <tbody>
       <tr>
-        <td>JUNIOR SISTEM ADMINISTRATOR</td>
+        <td>JUNIOR SISTEM ADMINISTRATOR &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
         <td>Rp 600.000 </td>
         <td>Rp 350.000</td>
         <td>Rp 650.000</td>
       </tr>
       <tr>
         <td>SISTEM ADMINISTRATOR</td>
-        <td>RP 1.000.000</td>
-        <td>RP 350.000</td>
-        <td>RP 950.000</td>
+        <td>Rp 1.000.000</td>
+        <td>Rp 350.000</td>
+        <td>Rp 950.000</td>
       </tr>
       <tr>
         <td>SISTEM ENGINEER</td>
-        <td>RP 2.000.000</td>
-        <td>RP 350.000</td>
-        <td>RP 950.000</td>
+        <td>Rp 2.000.000</td>
+        <td>Rp 350.000</td>
+        <td>Rp 950.000</td>
       </tr>
     </tbody>
   </table>
 </div>
 
-<div class="container">
+<div class="table-responsive">
 <h2 style="font-size: 15px;">6. MULTIMEDIA</h2>   
   <table class="table table-hover">
     <thead>
       <tr>
-        <th style="width:40%;">Sertifikasi</th>
-        <th style="width:20%;">Biaya Assesmen</th>
-        <th style="width:20%;">Biaya Sertifikasi</th>
-        <th style="width:20%;">Biaya Resertifikasi</th>
+        <th style="background-color: #1E90FF;">Sertifikasi</th>
+        <th style="background-color: #1E90FF;">Biaya Assesmen</th>
+        <th style="background-color: #1E90FF;">Biaya Sertifikasi</th>
+        <th style="background-color: #1E90FF;">Biaya Resertifikasi</th>
       </tr>
     </thead>
     <tbody>
       <tr>
-        <td>JUNIOR DIGITAL ARTIST</td>
+        <td>JUNIOR DIGITAL ARTIST &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
         <td>Rp 600.000 </td>
         <td>Rp 350.000</td>
         <td>Rp 650.000</td>
       </tr>
       <tr>
         <td>DIGITAL ARTIST</td>
-        <td>RP 1.000.000</td>
-        <td>RP 350.000</td>
-        <td>RP 950.000</td>
+        <td>Rp 1.000.000</td>
+        <td>Rp 350.000</td>
+        <td>Rp 950.000</td>
       </tr>
     </tbody>
   </table>
