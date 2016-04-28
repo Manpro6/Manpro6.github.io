f4736b102f874e7a84d9ec54a6e96ef9427d5085
Ervan Kurnia Sanjaya <ervan.sanjaya@ti.ukdw.ac.id>
2016-04-28 23:22:31 +0700

    Revisi tampilan Berita dan Lab @atas permintaan Hiro

--- application/views/berita/index.php
+++ application/views/berita/index.php
@@ -1,14 +1,19 @@
 <head><title>Admin Panel - Edit Berita</title></head>
 <div class="container">
   <h2>Daftar berita</h2><hr>
-  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a  type="button" href="<?php echo site_url('berita/add')?>" class="btn btn-xs btn-primary"><span class="glyphicon glyphicon-edit" data-toggle="modal" data-target="#modal-edit"></span><strong>Tambah Berita</strong></a><br><br>       
+  <div style="float: right; margin-right: 30px;">
+    <a href="<?php echo site_url('berita/add')?>" class="btn btn-xs btn-primary">
+      <button type='button' class='btn btn-primary' style="border: none;"><span class="glyphicon glyphicon-plus"></span> Tambah Berita</button>
+    </a>
+  </div>
+  <br><br>       
     <table class="table table-striped" style="margin: auto; width:70%; overflow:auto; text-align:center;">
     
       <tbody>
         <?php if($berita) : ?>
               <?php foreach ($berita as $mydata):?>
-              	<?php $isi = $mydata['isi']?>
-              	<?php 
+                <?php $isi = $mydata['isi']?>
+                <?php 
                 $length = 250;
                 $input = substr($isi, 0,250);
     
@@ -33,8 +38,11 @@
             <th style="text-align:justify;"><br><?php echo $input ?>......................</th>
             </td>
             <th style="text-align:justify;"><br><br><br>
-             <a type="button" href="<?php echo site_url('berita/ubah').'/'.$mydata['id_berita'] ?>" class="btn btn-xs btn-primary"><span class="glyphicon glyphicon-edit" data-toggle="modal" data-target="#modal-edit"></span>&nbsp; Ubah</a><br><br>
-             <a type="button" href="<?php echo site_url('berita/delete').'/'.$mydata['id_berita'] ?>" class="btn btn-sm btn-danger" onclick="return confirm('Anda yakin ingin menghapus berita ini?')"><span class="glyphicon glyphicon-trash" data-toggle="modal" data-target="#modal-delete"></span>&nbsp; Delete</a><br><br>
+             <a href="<?php echo site_url('berita/ubah').'/'.$mydata['id_berita'] ?>">
+             <button type='button' class='btn btn-info editButton' style="width: 90px;"><span class="glyphicon glyphicon-edit" data-toggle="modal" data-target="#modal-edit"></span>&nbsp;Ubah</button>
+             </a>
+             <a href="<?php echo site_url('berita/delete').'/'.$mydata['id_berita'] ?>">
+             <button type='button' class='btn btn-danger editButton' style="width: 90px;"><span class='glyphicon glyphicon-trash'></span>&nbsp;Hapus</button>
             </th>
             </tr>
               <?php endforeach; ?>
